f79e74a307335260035eebbd0c291d59dc88eea8
Matteo Bando <bandomatteo@gmail.com>
2024-08-17 16:26:43 +0200

    chore: Initialize project structure and configuration files

---
+++ src/test/java/com/bandomatteo/Prototipo_REST_Auth/PrototipoRestAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandomatteo.Prototipo_REST_Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototipoRestAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
