ac1c49b3f59319f9ef87fd0bf9128a602433916e
Matteo Bando <bandomatteo@gmail.com>
2024-08-21 12:11:13 +0200

    feat: Update requestMatchers in SecurityConfiguration to include /api/v1/auth/**
    feat: Implement register and authenticate methods in AuthenticationController
    
    feat: Implement register and authenticate methods in AuthenticationService
    
    feat: Add UserRepository, PasswordEncoder, JWTService, and AuthenticationManager to AuthenticationService
    
    feat: Implement register method in AuthenticationService
    
    feat: Implement authenticate method in AuthenticationService
    
    feat: Update register method in AuthenticationService to save user to UserRepository
    
    feat: Update authenticate method in AuthenticationService to authenticate user and generate JWT token
    
    feat: Add AuthenticationRequestDTO and AuthenticationResponseDTO classes
    
    feat: Add register and authenticate endpoints to AuthenticationController
    
    feat: Update request mapping in AuthenticationController to /api/v1/auth
    
    feat: Update register endpoint in AuthenticationController to return ResponseEntity with registered user's JWT token
    
    feat: Update authenticate endpoint in AuthenticationController to return ResponseEntity with authenticated user's JWT token

--- src/main/java/com/bandomatteo/Prototipo_REST_Auth/controllers/AuthenticationController.java
+++ src/main/java/com/bandomatteo/Prototipo_REST_Auth/controllers/AuthenticationController.java
@@ -7,12 +7,15 @@ import com.bandomatteo.Prototipo_REST_Auth.services.AuthenticationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping( "/api/v1/auth")
 public class AuthenticationController {
 
     AuthenticationService authenticationService;
+
     public AuthenticationController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
@@ -20,11 +23,11 @@ public class AuthenticationController {
 
     @PostMapping(name = "/register")
     public ResponseEntity<AuthenticationResponseDTO> register(@RequestBody RegisterRequestDTO request) {
-        //TODO
+        return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping(name = "/authenticate")
     public ResponseEntity<AuthenticationResponseDTO> authenticate(@RequestBody AuthenticationRequestDTO request) {
-        //TODO
+        return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 }
--- src/main/java/com/bandomatteo/Prototipo_REST_Auth/services/AuthenticationService.java
+++ src/main/java/com/bandomatteo/Prototipo_REST_Auth/services/AuthenticationService.java
@@ -1,7 +1,73 @@
 package com.bandomatteo.Prototipo_REST_Auth.services;
 
+import com.bandomatteo.Prototipo_REST_Auth.domain.dto.AuthenticationRequestDTO;
+import com.bandomatteo.Prototipo_REST_Auth.domain.dto.AuthenticationResponseDTO;
+import com.bandomatteo.Prototipo_REST_Auth.domain.dto.RegisterRequestDTO;
+import com.bandomatteo.Prototipo_REST_Auth.domain.entities.User.Role;
+import com.bandomatteo.Prototipo_REST_Auth.domain.entities.User.UserEntity;
+import com.bandomatteo.Prototipo_REST_Auth.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    private  JWTService jwtService;
+
+    private AuthenticationManager authenticationManager;
+
+
+
+    public AuthenticationService(UserRepository userRepository,
+                                 PasswordEncoder passwordEncoder,
+                                 JWTService jwtService,
+                                 AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    public AuthenticationResponseDTO register(RegisterRequestDTO request) {
+
+        UserEntity user = UserEntity
+                .builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+         var jwt = jwtService.generateToken(user);
+
+         return AuthenticationResponseDTO
+                 .builder()
+                 .token(jwt)
+                 .build();
+    }
+
+    public AuthenticationResponseDTO authenticate(AuthenticationRequestDTO request) {
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        UserEntity user = userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwt = jwtService.generateToken(user);
+
+        return AuthenticationResponseDTO
+                .builder()
+                .token(jwt)
+                .build();
+    }
 }
